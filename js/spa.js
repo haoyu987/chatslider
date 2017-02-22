8437e04b02ebe0697c96506253ef03e5ed5413df
Hao Yu <hao.yu.019@gmail.com>
2017-02-23 00:52:46 +0800

    Update spa.js

--- js/spa.js
+++ js/spa.js
@@ -6,7 +6,6 @@
 var spa = (function () {
 	'use strict';
 	var initModule = function ( $container ) {
-		spa.data.initModule();
 		spa.model.initModule();
 		spa.shell.initModule( $container );
 	};
